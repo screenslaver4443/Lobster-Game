9264b1b1d31736c68960c51c2cc0bff939082d41
Nikolai <npesudovs@outlook.com>
2022-09-06 11:29:04 +0930

    Made lobster have a sprite

--- LobsterGame.py
+++ LobsterGame.py
@@ -12,9 +12,11 @@ class lobster():
     def __init__(self):
         self.x = 100
         self.y = 100
-        self.speedX = 0
+        self.speedX = 0 
         self.speedY = 0
         self.size = 80
+        self.image = pygame.image.load("Assets/lobster.png") # load sprite image
+        self.image = pygame.transform.scale(self.image, (self.size, self.size)) #resizes sprite image
 
     def move(self):
         self.check_walls()
@@ -30,7 +32,10 @@ class lobster():
             self.speedY  = 0
     
     def draw(self, screen):
-        pygame.draw.rect(screen, WHITE, [lob.x, lob.y, lob.size, lob.size])
+        screen.blit(self.image, [self.x, self.y]) #draws sprite image
+
+
+
 
 ##### Colours #####
 # sourcery skip: merge-comparisons
@@ -45,23 +50,21 @@ size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Lobster Game")
 
-#Image loading
-background = pygame.image.load("assets/sand.jpg")
-
-#Transformations
+#Background loading and transforming
+background = pygame.image.load("assets/sand.jpg") #Load the background
 background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT)) #scales background to screen size
 
-lob  = lobster()
-done = False              
-clock = pygame.time.Clock()
+lob  = lobster() #makes lob equal to the class lobster
+done = False              #Prepares the quit variable
+clock = pygame.time.Clock() #prepares the clock variable
 
 ##### Main Program Loop #####
 while not done:
     ##### Events Loop #####
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN: #TODO: Put this in a separate function within the class lobster
             if event.key == pygame.K_LEFT:
                 lob.speedX = -3
             elif event.key == pygame.K_RIGHT:
@@ -76,7 +79,7 @@ while not done:
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 lob.speedY = 0
 
-    #Movement and drawing
+    #Movement 
     lob.move()
     
     #drawing
